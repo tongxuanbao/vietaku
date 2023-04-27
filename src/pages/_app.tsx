8edbea1dfb7209bdf9ba51109f6711691b831c79
Bao Tong <baotongxuan@gmail.com>
2023-04-28 03:03:19 +1000

    added clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { viVN } from "~/utils/vi-VN";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider localization={viVN} {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
