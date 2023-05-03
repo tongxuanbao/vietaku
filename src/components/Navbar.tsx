648c86104f1ae291ffd171d9824dc527491feac6
Bao Tong <baotongxuan@gmail.com>
2023-05-03 21:06:00 +1000

    href

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -147,7 +147,7 @@ export default function Navbar() {
               <div className="relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12">
                 <div className="flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2">
                   <div className="flex flex-shrink-0 items-center">
-                    <Link href="#">
+                    <Link href="/">
                       <Image
                         height={32}
                         width={160}
