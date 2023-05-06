3bb7bcd6500844f0d3978d25dbb5fba22f9040ac
Bao Tong <baotongxuan@gmail.com>
2023-05-06 14:21:23 +1000

    Signin singout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -29,6 +29,8 @@ import {
   ChevronDownIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
+import { SignOutButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 const navigation = [
   { name: "Trang Chủ", href: "#", icon: HomeIcon, current: true },
@@ -41,8 +43,12 @@ const teams = [
   { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
 ];
 const userNavigation = [
-  { name: "Your profile", href: "#" },
-  { name: "Sign out", href: "#" },
+  {
+    name: "Your profile",
+    href: "#",
+    component: <a href="/user">User Profile</a>,
+  },
+  { name: "Sign out", href: "#", component: <SignOutButton /> },
 ];
 
 function classNames(...classes: string[]) {
@@ -122,11 +128,13 @@ export default function Layout({ children }: LayoutProps) {
                   {/* Sidebar component, swap this element with another sidebar if you like */}
                   <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-zinc-900 px-6 pb-4 ring-1 ring-white/10">
                     <div className="flex h-16 shrink-0 items-center">
-                      <img
-                        className="h-8 w-auto"
-                        src="/logo.svg"
-                        alt="Your Company"
-                      />
+                      <Link href="/">
+                        <img
+                          className="h-8 w-auto"
+                          src="/logo.svg"
+                          alt="Your Company"
+                        />
+                      </Link>
                     </div>
                     <nav className="flex flex-1 flex-col">
                       <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -153,31 +161,6 @@ export default function Layout({ children }: LayoutProps) {
                             ))}
                           </ul>
                         </li>
-                        {/* <li>
-                          <div className="text-xs font-semibold leading-6 text-zinc-400">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-zinc-800 text-white"
-                                      : "text-zinc-400 hover:bg-zinc-800 hover:text-white",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                                  )}
-                                >
-                                  <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-zinc-700 bg-zinc-800 text-[0.625rem] font-medium text-zinc-400 group-hover:text-white">
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li> */}
                         <li className="mt-auto">
                           <a
                             href="#"
@@ -204,7 +187,13 @@ export default function Layout({ children }: LayoutProps) {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-zinc-900 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
-              <img className="h-8 w-auto" src="/logo.svg" alt="Your Company" />
+              <Link href="/">
+                <img
+                  className="h-8 w-auto"
+                  src="/logo.svg"
+                  alt="Your Company"
+                />
+              </Link>
             </div>
             <nav className="flex flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -232,31 +221,6 @@ export default function Layout({ children }: LayoutProps) {
                   </ul>
                 </li>
                 <li>
-                  <div className="text-xs font-semibold leading-6 text-zinc-400">
-                    Your teams
-                  </div>
-                  <ul role="list" className="-mx-2 mt-2 space-y-1">
-                    {teams.map((team) => (
-                      <li key={team.name}>
-                        <a
-                          href={team.href}
-                          className={classNames(
-                            team.current
-                              ? "bg-zinc-800 text-white"
-                              : "text-zinc-400 hover:bg-zinc-800 hover:text-white",
-                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                          )}
-                        >
-                          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-zinc-700 bg-zinc-800 text-[0.625rem] font-medium text-zinc-400 group-hover:text-white">
-                            {team.initial}
-                          </span>
-                          <span className="truncate">{team.name}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li className="mt-auto">
                   <a
                     href="#"
                     className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-zinc-400 hover:bg-zinc-800 hover:text-white"
@@ -337,7 +301,7 @@ export default function Layout({ children }: LayoutProps) {
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt=""
                     />
-                    <span className="hidden lg:flex lg:items-center">
+                    {/* <span className="hidden lg:flex lg:items-center">
                       <span
                         className="ml-4 text-sm font-semibold leading-6 text-white"
                         aria-hidden="true"
@@ -348,7 +312,7 @@ export default function Layout({ children }: LayoutProps) {
                         className="ml-2 h-5 w-5 text-zinc-400"
                         aria-hidden="true"
                       />
-                    </span>
+                    </span> */}
                   </Menu.Button>
                   <Transition
                     as={Fragment}
@@ -363,15 +327,16 @@ export default function Layout({ children }: LayoutProps) {
                       {userNavigation.map((item) => (
                         <Menu.Item key={item.name}>
                           {({ active }) => (
-                            <a
-                              href={item.href}
+                            <div
                               className={classNames(
                                 active ? "bg-zinc-700" : "",
                                 "block px-3 py-1 text-sm leading-6 text-white"
                               )}
                             >
-                              {item.name}
-                            </a>
+                              {/* <item.component /> */}
+                              {/* {item.name} */}
+                              {item.component}
+                            </div>
                           )}
                         </Menu.Item>
                       ))}
